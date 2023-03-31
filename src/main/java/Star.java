74596626875939fded88348b84ac00fd7f367f82
YeongChan23 <oscar7744@naver.com>
2023-03-31 14:45:57 +0900

    Star

---
+++ src/main/java/Star.java
@@ -0,0 +1,14 @@
+public class Star {
+    public static void main(String[] args) {
+        int i, k;
+
+        for (i = 0, k = 0; i <= 4; i++) {
+        System.out.printf("*");
+            {
+                if (k <= i)
+                    k++;
+            }
+        System.out.println("");
+        }
+    }
+}
