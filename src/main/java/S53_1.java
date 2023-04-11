ae529a36347a81464d01c8a92860902236ac5dd5
YeongChan23 <oscar7744@naver.com>
2023-04-11 23:05:27 +0900

    S53

---
+++ src/main/java/S53_1.java
@@ -0,0 +1,11 @@
+public class S53_1 {
+    public static void main(String[] args) {
+        int a[] = new int [5];
+        int i;
+
+            for(i = 0; i < 5; i++)
+                a[i] = i + 10;
+            for(i = 0; i < 5; i++)
+                System.out.printf(" %d", a[i]);
+    }
+}
---
+++ src/main/java/S53_2.java
@@ -0,0 +1,12 @@
+public class S53_2 {
+    public static void main(String[] args) {
+        int [] a ={90, 100, 80, 70, 60, 50, 30};
+        int hap = 0;
+        float avg;
+
+        for(int i : a)
+            hap = hap + i;
+        avg = (float)hap / a.length;
+        System.out.printf("%d, %2f", hap, avg);
+    }
+}
