e3bfbec3c6c8727f25c9fc089c5b9e6b041f2559
YeongChan23 <oscar7744@naver.com>
2023-04-12 03:10:53 +0900

    test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@ public class Test {
     public static void main(String[] args) {
         int arr[] = new int[5]; //5개 받을 수 있는 배열 생성
         int i;
-        int max = 0, min = 0,sum= 0;
+        int max = 0, min = 0, sum= 0;
         Scanner sc = new Scanner(System.in); //정수를 받을 수 있는 객체 생성
 
         System.out.printf("다섯개의 정수를 입력하세요.\n");
@@ -12,10 +12,11 @@ public class Test {
         for (i = 0; i < arr.length; i++) { //0부터 미리 선언해놓은 배열의 길이 5까지 정수를 입력 받음
             arr[i] = sc.nextInt();
             {
-                if (max < arr[i]) { //최대값
+                max = arr[0];      // 배열의 첫번째 원소로 초기화 해주지 않으면
+                min = arr[0];      // min에서 입력받았을때 0보다 큰값을 입력받으면 에러남
+                if (max< arr[i]) { //최대값
                     max = arr[i];
                 }
-
                 if (min > arr[i]) { //최소값
                     min = arr[i];
                 }
